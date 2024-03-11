1797fd5e5125f86145ff617a73db5bddc6ff552e
hasan ghanbari <hasanghanbari20@gmail.com>
2024-03-11 15:55:49 +0330

    add moment and jalali moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [showDropdown, setShowDropdown] = useState(false);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>
+        react Datepicker
+      </h1>
+      <>
+      <div className="date-picker">
+        <div className="inputs">
+          <div className="from-date">
+            <label htmlFor="exampleDataList" className="form-label">
+              از تاریخ:
+            </label>
+            <input
+              className="form-control"
+              list="datalistOptions"
+              id="exampleDataList"
+              placeholder=""
+              onClick={() => setShowDropdown(true)}
+            />
+          </div>
+          <div className="to-date">
+            <label htmlFor="exampleDataList" className="form-label">
+              تا تاریخ:
+            </label>
+            <input
+              className="form-control"
+              list="datalistOptions"
+              id="exampleDataList"
+              placeholder=""
+            />
+          </div>
+        </div>
+        <div className={`dropdown box`}>
+          <div className={`dropdown-menu p-4 text-body-secondary ${showDropdown ? `show` : ``}`}>
+            
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </>
     </>
   )
 }
